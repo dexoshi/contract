114827506cf51f1a707f194c02ba9dfdebb22bf7
leon-do <leondo123@gmail.com>
2023-09-12 22:19:46 -0400

    update meta

--- test/contractURI.ts
+++ test/contractURI.ts
@@ -12,7 +12,7 @@ describe("contractURI", function () {
       description:
         "Dexoshi is a Lens trading card game on the blockchain. There are a total of 255 unique cards and amount of individual cards can be infinite. But not all cards are equal...",
       image:
-        "https://ipfs.io/ipfs/bafkreiezcdvvipxiohdwm6hslbqwoy64lzg2vwjxi6rhdj7fcql5rwgo2q",
+        "https://ipfs.io/ipfs/bafybeiasmykcoebpnrhuuminmczqlvqeimuxtfwyuku2bh2mzi75satrgy/222.png",
       external_link: "https://www.lensfrens.xyz/dexoshi.lens",
       seller_fee_basis_points: 500, // Indicates a 5% seller fee.
       fee_recipient: signer[0].address,
